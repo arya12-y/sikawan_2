cbdb739d30bc01df9384292e99aa179abbfff696
Arya <aryahulopi12@gmail.com>
2026-07-13 09:55:20 +0800

    hero

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // TAMBAHKAN BLOK PREVIEW DI BAWAH INI
+  preview: {
+    allowedHosts: ['sikawan.up.railway.app', '.up.railway.app']
+  }
 })
